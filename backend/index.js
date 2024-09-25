fac24f20f4524ac221b827003c28acbfff6437f8
Robin Soni <sonirobin369@gmail.com>
2024-09-25 10:25:18 +0530

    Pub-sub redis connection added.

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,11 @@ import { Server } from "socket.io";
 import http from "http";
 import { addMsgToConversation } from "./controllers/msgs.controller.js";
 import msgsRouter from "./routes/msgs.route.js";
-import connectToMongoDB from "./db/connectTOMongoDB.js"; 
+import connectToMongoDB from "./db/connectTOMongoDB.js";
 import cors from 'cors';
+import { subscribe, publish } from "./redis/msgsPubSub.js";
+/* import Valkey from "ioredis"; */
+
 const app = express();
 app.use(cors());
 dotenv.config();
@@ -22,27 +25,49 @@ const userSocketMap = {};
 io.on('connection', (socket) => {
   const username = socket.handshake.query.username;
   console.log('Username of connected client:', username);
-  userSocketMap[username] = socket; 
+  userSocketMap[username] = socket;
+  const channelName = `chat_${username}`;
+  subscribe(channelName, (msg) => {
+    console.log('Received message:', msg);
+    socket.emit("chat msg", JSON.parse(msg));
+  });
+
   socket.on('chat msg', (msg) => {
+
     const receiverSocket = userSocketMap[msg.receiver];
-    socket.emit('chat msg', msg.text);
-    console.log(" socket server received msg from ", msg.sender, " for receiver ** ", msg.receiver ); 
     if (receiverSocket) {
       receiverSocket.emit('chat msg', msg);
-    } 
+    } else {
+      console.log(" checking the publish function of redis pub sub** ");
+      const channelName = `chat_${msg.receiver}`;
+      publish(channelName, JSON.stringify(msg));
+    }
+
     addMsgToConversation([msg.sender, msg.receiver], {
       text: msg.text,
       sender: msg.sender,
       receiver: msg.receiver
     });
   });
-}); 
+});
 
-app.use('/msgs',msgsRouter);
+app.use('/msgs', msgsRouter);
 // Define a route
 app.get('/', (req, res) => {
   res.send('Congratulations HHLD Folks!');
 });
+
+/** testing ioredis */ 
+/* const serviceUri = `rediss://${process.env.REDIS_USER}:AVNS_${process.env.REDIS_PWD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
+const valkey = new Valkey(serviceUri);
+
+valkey.set("key", "hello world");
+
+valkey.get("key").then(function (result) {
+    console.log(`The value of key is: ${result}`);
+    valkey.disconnect();
+}); */
+/** ** */
 
 // Start the server
 server.listen(PORT, (req, res) => {
